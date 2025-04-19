abdc5ea99910e37f5ffbc77a4ae4ab8e7a91986f
Kristen N. Finch <finchkn@uw.edu>
2025-04-18 17:33:38 -0700

    hyak bootcamp readme and materials added.

---
+++ hyak-bootcamp/basics/pi.py
@@ -0,0 +1,5 @@
+# Example Python script imports the Math library and prints the number pi. 
+import math
+print("Executing inside the container!")
+print("Pi=",math.pi)
+# Use `Ctrl+x` to exit the text editor. 
